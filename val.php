9948183da5c160e69f19c9417875d2f750795a7f
Misael-AC <99106212+Misael-AC@users.noreply.github.com>
2022-02-05 20:17:35 -0600

    Add files via upload

---
+++ val.php
@@ -0,0 +1,18 @@
+
+<?php 
+	session_start();
+
+	session_unset();
+
+	session_destroy();
+
+	header("Location: iniciar.html");
+
+	/*Cierra y la destruye la sesión  para
+	poder desconectar al usuario, después redirige
+	a index.php
+	==============================================
+	Close and destroys the session for disconnect
+	the user, then redirect to index.php*/
+?>
+?>
